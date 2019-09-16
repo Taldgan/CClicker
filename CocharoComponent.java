d8b4abf72ad1208e83558a80c7cf9c39c5635a4e
Taldgan <39274333+Taldgan@users.noreply.github.com>
2019-09-16 11:45:17 -0700

    Delete CocharoComponent.java

--- CocharoComponent.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Font;
-
-public class CocharoComponent extends InteractiveComponent {
-	public CocharoComponent(String path, int x, int y, int w, int h, String text, Font font, int tx, int ty, String id) {
-		super( path, x, y, w, h, text, font, tx, ty, id);
-	}
-}
