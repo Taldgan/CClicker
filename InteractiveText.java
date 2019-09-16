c5f0f7d69266dbce45658038d8c707ce31775d21
Taldgan <39274333+Taldgan@users.noreply.github.com>
2019-09-16 11:44:39 -0700

    Delete InteractiveText.java

--- InteractiveText.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Font;
-
-public class InteractiveText extends InteractiveComponent {
-	public InteractiveText(String text, Font font, int tx, int ty, int w, int h, String id) {
-		super(text, font, tx, ty, w, h, id);
-	}
-}
